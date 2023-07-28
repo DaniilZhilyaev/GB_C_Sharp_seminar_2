d5407efb39cc3564cd3cd9db566675728938791b
daniil <daniilzhilyaev@gmail.com>
2023-07-28 13:23:45 +0300

    add div 7 and 23  programm

---
+++ 4_div_7_23/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 3. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+14 -> нет
+46 -> нет
+161 -> да */
+
+Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(n);
+// int res = ((n - n % 100) / 100) + n % 10;
+// Console.Write((n - n % 100) / 100);
+// Console.WriteLine(n % 10);
+
+if (number1 % 7 == 0 && number1 % 23 == 0)
+{
+    Console.WriteLine($"Число {number1} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number1} не кратно 7 и 23");
+}
